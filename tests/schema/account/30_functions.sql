995949292dec649deb2dcb01942745c264ee76c9
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-12-04 10:12:28 +0100

    Add function without comment

--- tests/schema/account/30_functions.sql
+++ tests/schema/account/30_functions.sql
@@ -7,3 +7,10 @@ $$ LANGUAGE sql VOLATILE;
 COMMENT ON FUNCTION account.create_user(text, text) IS
 'Create a new user and return the new record';
 
+CREATE FUNCTION account.remove_user(name text)
+    RETURNS void
+AS $$
+DELETE FROM account.user WHERE name = $1;
+$$ LANGUAGE sql VOLATILE;
+
+-- Uncommented function
